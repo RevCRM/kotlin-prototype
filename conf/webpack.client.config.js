b2eff531fd4cab17ca2165ff51f75a3ecf257d24
Russell Briggs <russ@paraflyer.net>
2017-01-22 19:07:38 +1300

    Add static styles and grid system

--- conf/webpack.client.config.js
+++ conf/webpack.client.config.js
@@ -1,6 +1,7 @@
 var config = require('./webpack.base.config')();
 var webpack = require('webpack');
 var path = require('path');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 config.entry = './src/client/client.tsx';
 config.target = 'web';
@@ -8,7 +9,10 @@ config.output.filename = 'static/client.js';
 config.plugins.push(
     new webpack.DllReferencePlugin({
         manifest: require(path.join(config.output.path, 'static', 'clientlibs.json'))
-    })
+    }),
+    new CopyWebpackPlugin([
+        { from: 'static', to: 'static', context: './src' }
+    ])
 )
 
 module.exports = config;
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,6 @@
+
+declare module 'react-pure-grid';
+
 interface Window {
     __REDUX_DEVTOOLS_EXTENSION__: any;
 }
