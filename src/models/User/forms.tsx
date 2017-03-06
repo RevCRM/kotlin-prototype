443b28cc3c3951bf17338ddedd61118e5ad60510
Russell Briggs <russ@paraflyer.net>
2017-03-06 21:52:26 +1300

    Add stub API functions for login

--- src/models/User/forms.tsx
+++ src/models/User/forms.tsx
@@ -6,8 +6,5 @@ export const LoginForm = () => (
     <RevForm model="User" form="login_form">
         <RevField name="username" />
         <RevField name="password" />
-        <RevField name="active" />
-        <RevField name="user_type" />
-        <RevField name="age" />
     </RevForm>
 );
--- src/models/api.ts
+++ src/models/api.ts
@@ -3,4 +3,27 @@ import * as api from 'rev-api';
 
 import User from './User/User';
 
-api.register(User, { operations: 'all' });
+api.register(User, {
+    operations: 'all',
+    methods: [
+        {
+            name: 'login',
+            args: ['username', 'password'],
+            handler: (context, username, password) => {
+                if (username == 'admin' && password == 'admin') {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        },
+        {
+            name: 'logout',
+            args: [],
+            handler: (context) => {
+                return true;
+            }
+        }
+    ]
+});
