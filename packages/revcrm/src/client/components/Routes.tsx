6a6371e38160b16e045e2b60ba078f20ae84b00b
Russell Briggs <russ@paraflyer.net>
2018-07-02 08:23:42 +1200

    Typo and theme tweak

--- packages/revcrm/src/client/components/Routes.tsx
+++ packages/revcrm/src/client/components/Routes.tsx
@@ -1,11 +1,11 @@
 
 import * as React from 'react';
-import { createStyles, withStyles, WithStyles } from '@material-ui/core/styles';
+import { createStyles, withStyles, WithStyles, Theme } from '@material-ui/core/styles';
 import { Route } from 'react-router-dom';
 import { UserLoginForm } from '../../models/User/views/UserLoginForm';
 import { CRMViewManager } from '../../views/CRMViewManager';
 
-const styles = createStyles({
+const styles = (theme: Theme) => createStyles({
     root: {
         marginTop: 75,
         '@media print': {
