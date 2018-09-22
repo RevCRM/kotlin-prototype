9fa3f23ea0e19abf462fcf79ed8f48b46b6644ed
Russell Briggs <russ@paraflyer.net>
2018-09-23 00:20:10 +1200

    Created custom deserializer for SelectionOptions with database connection

--- src/main/kotlin/org/revcrm/data/DataLoader.kt
+++ src/main/kotlin/org/revcrm/data/DataLoader.kt
@@ -3,13 +3,13 @@ package org.revcrm.data
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import com.fasterxml.jackson.module.kotlin.KotlinModule
-import com.fasterxml.jackson.module.kotlin.readValue
 import org.revcrm.models.common.SelectionList
+import org.revcrm.models.common.SelectionOption
 import javax.inject.Inject
 
 data class SelectionListImport(
-    val selection_list: Array<SelectionList>
-//    val selection_option: Array<SelectionOption>
+    val selection_list: Array<SelectionList>,
+    val selection_option: Array<SelectionOption>
 )
 
 class DataLoader @Inject constructor(_db: Database, _inst: YMLHandlerInstantiator) {
@@ -19,12 +19,15 @@ class DataLoader @Inject constructor(_db: Database, _inst: YMLHandlerInstantiato
     fun importData() {
         val mapper = ObjectMapper(YAMLFactory())
         // https://stackoverflow.com/questions/28393599/autowiring-in-jsondeserializer-springbeanautowiringsupport-vs-handlerinstantiat
-        mapper.setHandlerInstantiator(inst)
+//        mapper.setHandlerInstantiator(inst)
         mapper.registerModule(KotlinModule())
 
         val res = object {}.javaClass.getResource("/data/selections.yml")
 
-        val import = mapper.readValue<SelectionListImport>(res)
+        val reader = mapper.reader()
+                .forType(SelectionListImport::class.java)
+                .withAttribute("db", db)
+        val import = reader.readValue<SelectionListImport>(res)
 
         db.withTransaction { session ->
             import.selection_list.forEach { list ->
--- src/main/kotlin/org/revcrm/models/common/Selections.kt
+++ src/main/kotlin/org/revcrm/models/common/Selections.kt
@@ -1,9 +1,16 @@
 package org.revcrm.models.common
 
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import org.revcrm.data.Database
 import org.revcrm.models.BaseModel
 import javax.persistence.Entity
 import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
+
 
 @Entity
 data class SelectionList(
@@ -12,6 +19,7 @@ data class SelectionList(
 ): BaseModel()
 
 @Entity
+@JsonDeserialize(using = SelectionOptionDeserializer::class)
 data class SelectionOption(
 
     @ManyToOne
@@ -22,3 +30,32 @@ data class SelectionOption(
     val seq: Short
 
 ): BaseModel()
+
+class SelectionOptionDeserializer : JsonDeserializer<SelectionOption>() {
+//    val db: Database = _db
+
+    override fun deserialize(jp: JsonParser, ctx: DeserializationContext): SelectionOption {
+        val db = ctx.getAttribute("db") as Database
+        val node = jp.codec.readTree<JsonNode>(jp)
+
+        val code = node.get("code").asText()
+        val label = node.get("label").asText()
+        val seq = node.get("seq").asInt().toShort()
+
+        val listId = node.get("list").asText()
+        println("Looking for list id '${listId}'")
+
+        val list = db.withTransaction {session ->
+            session.createQuery("""
+                    from SelectionList
+                    where dataId = :listId
+                """)
+                .setParameter("listId", listId)
+                .uniqueResult()
+        }
+        println("Got List ${list}")
+
+        val dummyList = SelectionList(model = "test", label = "test")
+        return SelectionOption(dummyList, code, label, seq)
+    }
+}
