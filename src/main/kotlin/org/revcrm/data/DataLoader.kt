258d45e091b200c65f166423194c5416649d67b6
Russell Briggs <russ@paraflyer.net>
2018-09-30 16:07:24 +1300

    Moved import routine next to model

--- src/main/kotlin/org/revcrm/data/DataLoader.kt
+++ src/main/kotlin/org/revcrm/data/DataLoader.kt
@@ -1,49 +1,12 @@
 package org.revcrm.data
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
-import org.revcrm.models.common.SelectionList
+import org.revcrm.models.common.importSelectionLists
 import javax.inject.Inject
 
 class DataLoader @Inject constructor(_db: Database) {
     private var db = _db
 
     fun importData() {
-        val mapper = ObjectMapper(YAMLFactory())
-//        mapper.registerModule(KotlinModule())
-
-        val res = object {}.javaClass.getResource("/data/selection_list.yml")
-
-        val nodes = mapper.readTree(res)
-        if (!nodes.isArray()) {
-            throw Exception("YAML Root Element must be an array")
-        }
-
-        db.withTransaction { session ->
-            for (node in nodes) {
-                val dataId = node.get("dataId").asText()
-                val model = node.get("model").asText()
-                val label = node.get("label").asText()
-
-                val existingRecord = session
-                    .bySimpleNaturalId(SelectionList::class.java)
-                    .load(dataId)
-
-                if (existingRecord != null) {
-                    println("Updating Existing Record")
-                    existingRecord.model = model
-                    existingRecord.label = label
-                }
-                else {
-                    println("Creating New Record")
-                    val newRecord = SelectionList(
-                            model = model,
-                            label = label
-                    )
-                    newRecord.dataId = dataId
-                    session.persist(newRecord)
-                }
-            }
-        }
+        importSelectionLists("/data/selection_list.yml", db)
     }
 }
--- src/main/kotlin/org/revcrm/models/common/SelectionList.kt
+++ src/main/kotlin/org/revcrm/models/common/SelectionList.kt
@@ -1,5 +1,8 @@
 package org.revcrm.models.common
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import org.revcrm.data.Database
 import org.revcrm.models.BaseModel
 import javax.persistence.Entity
 
@@ -8,3 +11,36 @@ data class SelectionList (
         var model: String,
         var label: String
 ): BaseModel()
+
+fun importSelectionLists(fileName: String, db: Database) {
+    val mapper = ObjectMapper(YAMLFactory())
+    val res = object {}.javaClass.getResource(fileName)
+    val nodes = mapper.readTree(res)
+    if (!nodes.isArray()) {
+        throw Exception("YAML Root Element must be an array")
+    }
+    db.withTransaction { session ->
+        for (node in nodes) {
+            val dataId = node.get("dataId").asText()
+            val model = node.get("model").asText()
+            val label = node.get("label").asText()
+
+            val existingRecord = session
+                .bySimpleNaturalId(SelectionList::class.java)
+                .load(dataId)
+
+            if (existingRecord != null) {
+                existingRecord.model = model
+                existingRecord.label = label
+            }
+            else {
+                val newRecord = SelectionList(
+                    model = model,
+                    label = label
+                )
+                newRecord.dataId = dataId
+                session.persist(newRecord)
+            }
+        }
+    }
+}
