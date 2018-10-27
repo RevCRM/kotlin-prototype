55158f979454f98da01c0d3fc70f644296425ce9
Russell Briggs <russ@paraflyer.net>
2018-10-27 22:35:05 +1300

    Add Google OAuth User Authentication

--- revcrm-server/src/main/kotlin/org/revcrm/Application.kt
+++ revcrm-server/src/main/kotlin/org/revcrm/Application.kt
@@ -3,6 +3,10 @@ package org.revcrm
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
+import io.ktor.auth.Authentication
+import io.ktor.auth.oauth
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.apache.Apache
 import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
 import io.ktor.features.DefaultHeaders
@@ -13,8 +17,17 @@ import io.ktor.locations.KtorExperimentalLocationsAPI
 import io.ktor.locations.Locations
 import io.ktor.response.respond
 import io.ktor.routing.routing
+import io.ktor.sessions.SessionTransportTransformerMessageAuthentication
+import io.ktor.sessions.Sessions
+import io.ktor.sessions.cookie
+import io.ktor.util.KtorExperimentalAPI
+import io.ktor.util.hex
 import org.koin.ktor.ext.installKoin
 import org.koin.log.Logger.SLF4JLogger
+import org.revcrm.auth.RevCRMSession
+import org.revcrm.auth.googleLogin
+import org.revcrm.auth.googleOauthProvider
+import org.revcrm.auth.redirectUrl
 import org.revcrm.server.graphQL
 import org.revcrm.server.graphiQL
 import org.revcrm.server.healthCheck
@@ -26,6 +39,7 @@ import java.text.DateFormat
  * set the "Main class" to "io.ktor.server.netty.EngineMain"
  */
 
+@KtorExperimentalAPI
 @KtorExperimentalLocationsAPI
 fun Application.main() {
 
@@ -33,6 +47,19 @@ fun Application.main() {
 
     install(DefaultHeaders) {
         header("Server", "RevCRM")
+    }
+    install(Sessions) {
+        cookie<RevCRMSession>("revCRMSessionId") {
+            val secretSignKey = hex(System.getenv("REVCRM_COOKIE_SIGN_KEY"))
+            transform(SessionTransportTransformerMessageAuthentication(secretSignKey))
+        }
+    }
+    install(Authentication) {
+        oauth("google-oauth") {
+            client = HttpClient(Apache)
+            providerLookup = { googleOauthProvider }
+            urlProvider = { redirectUrl("/login-google") }
+        }
     }
     install(CallLogging)
     install(ContentNegotiation) {
@@ -53,6 +80,7 @@ fun Application.main() {
     }
 
     routing {
+        googleLogin()
         staticFiles()
         graphQL()
         graphiQL()
