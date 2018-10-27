119b3fa08d3e7830af315ed898dfbb80c5c3c358
Russell Briggs <russ@paraflyer.net>
2018-10-27 15:34:01 +1300

    Support for static files served at /static from resources/www/static

--- revcrm-server/src/main/kotlin/org/revcrm/server/StaticFiles.kt
+++ revcrm-server/src/main/kotlin/org/revcrm/server/StaticFiles.kt
@@ -2,6 +2,8 @@ package org.revcrm.server
 
 import io.ktor.application.call
 import io.ktor.http.ContentType
+import io.ktor.http.content.resources
+import io.ktor.http.content.static
 import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import io.ktor.routing.get
@@ -14,8 +16,8 @@ fun Routing.staticFiles() {
         call.respondText(html, ContentType.Text.Html)
     }
 
-    get("/static") {
-        call.respondText("Static assets will go here...", ContentType.Text.Plain)
+    static("static") {
+        resources("/www/static")
     }
 
 }
