ff358ce642231db5e222794e2679ea88a62f226e
Russell Briggs <russ@paraflyer.net>
2018-12-06 18:10:55 +1300

    Improved Graph display and add tasks list

--- revcrm-client/src/components/dashboard/Dashboard.tsx
+++ revcrm-client/src/components/dashboard/Dashboard.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Typography, Grid, Card, CardHeader, CardContent } from '@material-ui/core';
+import { Typography, Grid, Card, CardHeader, CardContent, TableHead, Table, TableCell, Checkbox, TableBody, TableRow, IconButton, Icon } from '@material-ui/core';
 import { Bar } from 'britecharts-react';
 import withResponsiveness from 'britecharts-react/lib/esm/helpers/withResponsiveness';
 import { colors } from 'britecharts';
@@ -8,15 +8,15 @@ const ResponsiveBarChart = withResponsiveness(Bar);
 
 const oppoData = [
     {
-        value: 5500,
+        value: 5000000,
         name: 'New Lead'
     },
     {
-        value: 10000,
+        value: 10000000,
         name: 'Qualified'
     },
     {
-        value: 8500,
+        value: 8500000,
         name: 'Negotiating'
     },
 ];
@@ -29,29 +29,84 @@ export const Dashboard = () => (
             </Grid>
             <Grid item xs={12} md={6}>
                 <Card>
-                    <CardHeader title="Open Opportunities by Stage" />
-                    <CardContent>
-                        <ResponsiveBarChart
-                            data={oppoData}
-                            height={300}
-                            enableLabels={true}
-                            labelsNumberFormat=","
-                            isHorizontal={false}
-                            colorSchema={colors.colorSchemas.britecharts}
-                        />
-                    </CardContent>
+                    <CardHeader
+                        title="Open Opportunities by Stage"
+                        action={
+                            <IconButton>
+                                <Icon>more_vert</Icon>
+                            </IconButton>
+                        }
+                    />
+                    <ResponsiveBarChart
+                        data={oppoData}
+                        height={300}
+                        enableLabels={true}
+                        labelsNumberFormat=","
+                        isHorizontal={false}
+                        colorSchema={colors.colorSchemas.britecharts}
+                        margin={{ left: 90, right: 20, top: 20, bottom: 40 }}
+                    />
                 </Card>
             </Grid>
             <Grid item xs={12} md={6}>
                 <Card>
-                    <CardHeader title="Todays Tasks" />
+                    <CardHeader
+                        title="Todays Tasks"
+                        action={
+                            <IconButton>
+                                <Icon>more_vert</Icon>
+                            </IconButton>
+                        }
+                    />
                     <CardContent>
-                        <Typography color="textSecondary" gutterBottom>
-                        Task 1
-                        </Typography>
-                        <Typography color="textSecondary">
-                        Task 2
-                        </Typography>
+                        <Table padding="none">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell />
+                                    <TableCell>
+                                        Task
+                                    </TableCell>
+                                    <TableCell>
+                                        Related To
+                                    </TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                <TableRow>
+                                    <TableCell padding="checkbox">
+                                        <Checkbox />
+                                    </TableCell>
+                                    <TableCell>
+                                        Prepare Proposal
+                                    </TableCell>
+                                    <TableCell>
+                                        Opportunity 105: New Build
+                                    </TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell padding="checkbox">
+                                        <Checkbox />
+                                    </TableCell>
+                                    <TableCell>
+                                        Prepare Proposal
+                                    </TableCell>
+                                    <TableCell>
+                                        Opportunity 107: Cable Street Works
+                                    </TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell padding="checkbox">
+                                        <Checkbox />
+                                    </TableCell>
+                                    <TableCell>
+                                        Buy Milk
+                                    </TableCell>
+                                    <TableCell>
+                                        Personal
+                                    </TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
                     </CardContent>
                 </Card>
             </Grid>
