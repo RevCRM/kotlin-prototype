f0a98d9fb7ba680fe17a1312b47aee2b6a02979c
Russell Briggs <russ@paraflyer.net>
2018-12-13 10:51:51 +1300

    Initial work on Search Screen

--- revcrm-client/src/modules/revcrm-accounts/components/AccountList.tsx
+++ revcrm-client/src/modules/revcrm-accounts/components/AccountList.tsx
@@ -1,6 +1,110 @@
 
 import * as React from 'react';
+import { Typography, Paper, Input, InputAdornment, Icon, Theme, createStyles, withStyles, WithStyles, Table, TableHead, TableRow, TableCell, TableBody } from '@material-ui/core';
+import { fade } from '@material-ui/core/styles/colorManipulator';
 
-export const AccountList = () => (
-    <h2>Account List</h2>
-);
+export const styles = (theme: Theme) => createStyles({
+    root: {
+        padding: 12,
+        display: 'flex',
+        color: '#fff',
+        backgroundColor: theme.palette.primary.dark
+    },
+    searchBox: {
+        [theme.breakpoints.up('md')]: {
+            minWidth: 350,
+        },
+    },
+    searchInput: {
+        color: '#fff',
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+    },
+    searchAdornment: {
+        marginLeft: 4
+    },
+});
+
+export const AccountList = withStyles(styles)((props: WithStyles<typeof styles>) => (
+    <div>
+        <Paper square className={props.classes.root}>
+            <Typography variant="h6" color="inherit" style={{ flexGrow: 1 }}>
+                Accounts
+            </Typography>
+            <Input
+                className={props.classes.searchBox}
+                classes={{
+                    root: props.classes.searchInput,
+                }}
+                disableUnderline={true}
+                placeholder="Search"
+                startAdornment={
+                    <InputAdornment position="start" className={props.classes.searchAdornment}>
+                        <Icon>search</Icon>
+                    </InputAdornment>
+                }
+            />
+        </Paper>
+        <Table padding="none">
+            <TableHead>
+                <TableRow>
+                    <TableCell>
+                        Task
+                    </TableCell>
+                    <TableCell>
+                        Related To
+                    </TableCell>
+                    <TableCell>
+                        Related To
+                    </TableCell>
+                    <TableCell>
+                        Related To
+                    </TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                <TableRow>
+                    <TableCell>
+                        Prepare Proposal
+                    </TableCell>
+                    <TableCell>
+                        Opportunity 105: New Build
+                    </TableCell>
+                    <TableCell>
+                        Opportunity 105: New Build
+                    </TableCell>
+                    <TableCell>
+                        Opportunity 105: New Build
+                    </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell>
+                        Prepare Proposal
+                    </TableCell>
+                    <TableCell>
+                        Opportunity 107: Cable Street Works
+                    </TableCell>
+                    <TableCell>
+                        Opportunity 107: Cable Street Works
+                    </TableCell>
+                    <TableCell>
+                        Opportunity 107: Cable Street Works
+                    </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell>
+                        Buy Milk
+                    </TableCell>
+                    <TableCell>
+                        Personal
+                    </TableCell>
+                    <TableCell>
+                        Personal
+                    </TableCell>
+                    <TableCell>
+                        Personal
+                    </TableCell>
+                </TableRow>
+            </TableBody>
+        </Table>
+    </div>
+));
