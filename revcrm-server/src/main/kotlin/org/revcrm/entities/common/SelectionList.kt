7ec7b1a270548db8866f35252c32af55b82570c8
Russell Briggs <russ@paraflyer.net>
2019-01-10 20:51:39 +1300

    Remove old SelectionList import code

--- revcrm-server/src/main/kotlin/org/revcrm/entities/common/SelectionList.kt
+++ revcrm-server/src/main/kotlin/org/revcrm/entities/common/SelectionList.kt
@@ -17,35 +17,3 @@ class SelectionOption(
     var code: String,
     var label: String
 ) : Base()
-
-// fun importSelectionLists(fileName: String, db: DBService) {
-//    val mapper = ObjectMapper(YAMLFactory())
-//    val res = object {}.javaClass.getResource(fileName)
-//    val nodes = mapper.readTree(res)
-//    if (!nodes.isArray()) {
-//        throw Exception("YAML Root Element must be an array")
-//    }
-//    db.withTransaction { em ->
-//        for (node in nodes) {
-//            val code = node.get("code").asText()
-//            val model = node.get("model").asText()
-//            val label = node.get("label").asText()
-//
-//            val existingRecord = em.session
-//                .bySimpleNaturalId(SelectionList::class.java)
-//                .load(code)
-//
-//            if (existingRecord != null) {
-//                existingRecord.model = model
-//                existingRecord.label = label
-//            } else {
-//                val newRecord = SelectionList(
-//                    code = code,
-//                    model = model,
-//                    label = label
-//                )
-//                em.persist(newRecord)
-//            }
-//        }
-//    }
-// }
