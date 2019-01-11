b44cfc6864dca705afa7223768f1b796c3dde354
Russell Briggs <russ@paraflyer.net>
2019-01-12 11:40:48 +1300

    Add JSR380 annotation validation support for Morphia

--- revcrm-server/src/main/kotlin/org/revcrm/db/EntityValidator.kt
+++ revcrm-server/src/main/kotlin/org/revcrm/db/EntityValidator.kt
@@ -1,22 +1,22 @@
 package org.revcrm.db
 
-import xyz.morphia.mapping.MappedField
 import com.mongodb.DBObject
 import xyz.morphia.AbstractEntityInterceptor
 import xyz.morphia.mapping.Mapper
+import javax.validation.Validation
+import javax.validation.Validator
 
 class EntityValidator : AbstractEntityInterceptor() {
+    val validator: Validator
 
-    override fun prePersist(ent: Any, dbObj: DBObject, mapper: Mapper) {
-        val mc = mapper.getMappedClass(ent)
-        val fieldsToTest = mc.persistenceFields // mc.getFieldsAnnotatedWith(NonNull::class.java)
-        for (mf in fieldsToTest) {
-            if (mf.getFieldValue(ent) == null) {
-                throw ValidationException(mf)
-            }
-        }
+    init {
+        val factory = Validation.buildDefaultValidatorFactory()
+        validator = factory.validator
     }
 
-    internal class ValidationException(mf: MappedField) :
-        RuntimeException("field is not valid " + mf.fullName)
+    override fun prePersist(ent: Any, dbObj: DBObject, mapper: Mapper) {
+        val violations = validator.validate(ent)
+        if (!violations.isEmpty())
+            throw EntityValidationException(violations)
+    }
 }
--- revcrm-server/src/main/kotlin/org/revcrm/entities/RevUser.kt
+++ revcrm-server/src/main/kotlin/org/revcrm/entities/RevUser.kt
@@ -8,13 +8,16 @@ import javax.validation.constraints.NotBlank
 @Entity
 class RevUser(
 
-    @Label("First Name") @NotBlank
+    @Label("First Name")
+    @field:NotBlank
     var first_name: String,
 
-    @Label("Last Name") @NotBlank
+    @Label("Last Name")
+    @field:NotBlank
     var last_name: String,
 
-    @Label("E-mail Address") @NotBlank
+    @Label("E-mail Address")
+    @field:NotBlank
     var email: String,
 
     var last_login: LocalDateTime? = null
