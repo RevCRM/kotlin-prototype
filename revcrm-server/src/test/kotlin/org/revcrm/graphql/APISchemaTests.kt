fbca2b5d916179ce28cbed6f85745ea6f85b08c8
Russell Briggs <russ@paraflyer.net>
2019-01-22 17:05:17 +1300

    Vastly simplify APISchemaTests (dont manually construct metadata)

--- revcrm-server/src/test/kotlin/org/revcrm/graphql/APISchemaTests.kt
+++ revcrm-server/src/test/kotlin/org/revcrm/graphql/APISchemaTests.kt
@@ -9,125 +9,85 @@ import graphql.schema.GraphQLObjectType
 import io.mockk.every
 import io.mockk.mockkObject
 import org.assertj.core.api.Assertions.assertThat
+import org.bson.types.ObjectId
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.revcrm.annotations.APIDisabled
 import org.revcrm.db.DBService
 import org.revcrm.graphql.types.GraphQLDate
 import org.revcrm.graphql.types.GraphQLDateTime
 import org.revcrm.graphql.types.GraphQLObjectID
 import org.revcrm.graphql.types.GraphQLTime
-import org.revcrm.meta.Entity
 import org.revcrm.meta.MetadataService
-import org.revcrm.meta.fields.BooleanField
-import org.revcrm.meta.fields.DateField
-import org.revcrm.meta.fields.DateTimeField
-import org.revcrm.meta.fields.DecimalField
-import org.revcrm.meta.fields.EnumField
-import org.revcrm.meta.fields.FloatField
-import org.revcrm.meta.fields.IDField
-import org.revcrm.meta.fields.IField
-import org.revcrm.meta.fields.IntegerField
-import org.revcrm.meta.fields.RelatedEntityField
-import org.revcrm.meta.fields.TextField
-import org.revcrm.meta.fields.TimeField
+import org.revcrm.testdb.EnumFieldOptions
+import xyz.morphia.annotations.Id
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+
+@APIDisabled
+class SensitiveEntity(
+    @Id
+    var id: ObjectId,
+    var name: String
+)
+
+class TestFieldsEntity(
+    @Id
+    var id_field: ObjectId,
+    var int_field: Int,
+    var short_field: Short,
+    var long_field: Long,
+    var float_field: Float,
+    var double_field: Double,
+    var decimal_field: BigDecimal,
+    var boolean_field: Boolean,
+    var string_field: String,
+    var date_field: LocalDate,
+    var time_field: LocalTime,
+    var datetime_field: LocalDateTime,
+    var enum_field: EnumFieldOptions,
+    var related_field: TestConstraintsEntity,
+    @APIDisabled
+    var api_disabled_field: String
+)
+
+class TestConstraintsEntity(
+    @Id
+    var id: ObjectId,
+    var nullable_field: String?,
+    var non_nullable_field: String
+)
+
+val entityClasses = listOf(
+    "org.revcrm.graphql.SensitiveEntity",
+    "org.revcrm.graphql.TestFieldsEntity",
+    "org.revcrm.graphql.TestConstraintsEntity"
+)
+val embeddedClasses = listOf<String>()
+
+val enabledEntities = 2
+val metadataNodes = 1 // query { Metadata }
 
 class APISchemaTests {
 
     /**
-     * Create mock metadata
-     */
-    val entities = listOf<Entity>(
-        Entity(
-            name = "SensitiveEntity",
-            idField = "id",
-            apiEnabled = false,
-            className = "test.SensitiveEntity",
-            fields = mapOf(
-                "name" to TextField(
-                    name = "name", label = "Name", apiEnabled = true,
-                    jvmType = "java.lang.String",
-                    nullable = false, properties = mapOf(),
-                    constraints = mapOf()
-                )
-            )
-        ),
-        Entity(
-            name = "TestFieldsEntity",
-            idField = "id",
-            apiEnabled = true,
-            className = "test.TestFieldsEntity",
-            fields = mapOf<String, IField>(
-                "id_field" to IDField(name = "id_field", label = "ID", apiEnabled = true,
-                    jvmType = "org.bson.types.ObjectId", nullable = false),
-                "int_field" to IntegerField(name = "int_field", label = "Integer Field", apiEnabled = true,
-                    jvmType = "int", nullable = false, properties = mapOf(), constraints = mapOf()),
-                "short_field" to IntegerField(name = "short_field", label = "Short Field", apiEnabled = true,
-                    jvmType = "short", nullable = false, properties = mapOf(), constraints = mapOf()),
-                "long_field" to IntegerField(name = "long_field", label = "Long Field", apiEnabled = true,
-                    jvmType = "long", nullable = false, properties = mapOf(), constraints = mapOf()),
-                "float_field" to FloatField(name = "float_field", label = "Float Field", apiEnabled = true,
-                    jvmType = "float", nullable = false, properties = mapOf(), constraints = mapOf()),
-                "double_field" to FloatField(name = "double_field", label = "Double Field", apiEnabled = true,
-                    jvmType = "double", nullable = false, properties = mapOf(), constraints = mapOf()),
-                "decimal_field" to DecimalField(name = "decimal_field", label = "Decimal Field", apiEnabled = true,
-                    jvmType = "java.math.BigDecimal", nullable = false, properties = mapOf(), constraints = mapOf()),
-                "boolean_field" to BooleanField(name = "boolean_field", label = "Boolean Field", apiEnabled = true,
-                    jvmType = "boolean", nullable = false),
-                "string_field" to TextField(name = "string_field", label = "String Field", apiEnabled = true,
-                    jvmType = "java.lang.String", nullable = false, properties = mapOf(), constraints = mapOf()),
-                "date_field" to DateField(name = "date_field", label = "Date Field", apiEnabled = true,
-                    jvmType = "java.time.LocalDate", nullable = false, properties = mapOf(), constraints = mapOf()),
-                "time_field" to TimeField(name = "time_field", label = "Time Field", apiEnabled = true,
-                    jvmType = "java.time.LocalTime", nullable = false, properties = mapOf(), constraints = mapOf()),
-                "datetime_field" to DateTimeField(name = "datetime_field", label = "DateTime Field", apiEnabled = true,
-                    jvmType = "java.time.LocalDateTime", nullable = false, properties = mapOf(), constraints = mapOf()),
-                "enum_field" to EnumField(name = "enum_field", label = "Enum Field", apiEnabled = true,
-                    jvmType = "org.revcrm.testdb.EnumFieldOptions", nullable = false),
-                "related_field" to RelatedEntityField(name = "related_field", label = "RelatedEntity Field", apiEnabled = true,
-                    jvmType = "org.revcrm.testdb.TestConstraintsEntity", nullable = false, relatedEntity = "TestConstraintsEntity"),
-                "api_disabled_field" to TextField(name = "api_disabled_field", label = "Disabled Field", apiEnabled = false,
-                    jvmType = "java.lang.String", nullable = false, properties = mapOf(), constraints = mapOf())
-            )
-        ),
-        Entity(
-            name = "TestConstraintsEntity",
-            idField = "id",
-            apiEnabled = true,
-            className = "test.TestConstraintsEntity",
-            fields = mapOf<String, IField>(
-                "nullable_field" to TextField(name = "nullable_field", label = "Nullable Text Field", apiEnabled = true,
-                    jvmType = "java.lang.String", nullable = true, properties = mapOf(), constraints = mapOf()
-                ),
-                "non_nullable_field" to TextField(name = "non_nullable_field", label = "Non-nullable Text Field", apiEnabled = true,
-                    jvmType = "java.lang.String", nullable = false, properties = mapOf(), constraints = mapOf()
-                )
-            )
-        )
-    )
-    val enabledEntities = 2
-    val metadataNodes = 1 // query { Metadata }
-
-    /**
-     * Create mock DBService and MetadataService
+     * Create mock DBService
      */
     val data = DBService().apply {
         mockkObject(this)
     }
-
-    val meta = MetadataService(data).apply {
-        mockkObject(this)
-    }
     init {
-        every { meta.getEntities() } returns entities
-        every { meta.getEmbeddedEntities() } returns listOf()
+        every { data.getEntityClassNames() } returns entityClasses
+        every { data.getEmbeddedClassNames() } returns embeddedClasses
     }
 
     /**
-     * Instantiate APIService using mock data sources
+     * Instantiate MetadataService and APIService
      */
-    val schema = APIService(data, meta).apply {
-        initialise()
-    }
+    val meta = MetadataService(data).apply { initialise() }
+    val schema = APIService(data, meta).apply { initialise() }
 
     val queryType = schema.graphQLSchema.queryType
     val testFieldsEntity = queryType.getFieldDefinition("TestFieldsEntity")
