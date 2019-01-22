7fe9d891cc074656875d0b94cea99916e0d71ec7
Russell Briggs <russ@paraflyer.net>
2019-01-23 11:54:11 +1300

    Remove unneeded scripts

--- revcrm-client/scripts/hash-pw.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcrypt');
-
-const pwToHash = process.argv[2];
-console.log('Plaintext:', pwToHash);
-
-(async () => {
-    const hash = await bcrypt.hash(pwToHash, 10);
-    console.log('Bcrypt Hash:', hash);
-})();
