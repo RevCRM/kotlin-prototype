5abab6fcf9adfdec49d9260a10fffc2156305f85
Russell Briggs <russ@paraflyer.net>
2019-01-24 12:58:48 +1300

    Make address fields nullable (except "name")

--- revcrm-server/src/main/kotlin/org/revcrm/entities/accounts/Address.kt
+++ revcrm-server/src/main/kotlin/org/revcrm/entities/accounts/Address.kt
@@ -3,29 +3,31 @@ package org.revcrm.entities.accounts
 import org.revcrm.annotations.EmbeddedEntity
 import org.revcrm.annotations.Label
 import org.revcrm.annotations.MultiLine
+import javax.validation.constraints.NotBlank
 
 @EmbeddedEntity
 class Address(
 
     @Label("Description")
+    @field:NotBlank
     var name: String,
 
     @Label("Address")
     @MultiLine
-    var address: String,
+    var address: String?,
 
     @Label("City")
-    var city: String,
+    var city: String?,
 
     @Label("Region")
-    var region: String,
+    var region: String?,
 
     @Label("Postal Code")
-    var postal_code: String,
+    var postal_code: String?,
 
     @Label("Country")
-    var country: String
+    var country: String?
 ) {
     val record_name: String
-    get() = "${address.lines().first()}, ${this.city} (${this.name})"
+    get() = "${address?.lines()?.first() ?: "No Address"}, ${this.city} (${this.name})"
 }
