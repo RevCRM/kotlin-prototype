5f2a0f357869373f3e67e35506ebfd41f04ef379
Russell Briggs <russ@paraflyer.net>
2019-02-27 10:45:02 +1300

    Re-enable GraphiQL UI

--- revcrm-server/src/main/kotlin/org/revcrm/config/WebConfiguration.kt
+++ revcrm-server/src/main/kotlin/org/revcrm/config/WebConfiguration.kt
@@ -1,7 +1,15 @@
 package org.revcrm.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebConfiguration : WebMvcConfigurer
+class WebConfiguration : WebMvcConfigurer {
+
+    override fun addViewControllers(registry: ViewControllerRegistry) {
+        registry
+            .addViewController("/graphiql")
+            .setViewName("forward:/graphiql/index.html")
+    }
+}
--- revcrm-server/src/main/kotlin/org/revcrm/controllers/HelloSpringController.kt
+++ revcrm-server/src/main/kotlin/org/revcrm/controllers/HelloSpringController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class HelloSpringController {
 
-    @GetMapping("/")
+    @GetMapping("/test")
     fun hello(): String {
         return "Hello Spring!"
     }
